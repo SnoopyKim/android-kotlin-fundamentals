c752e39a8115bcdcc12411fcfb73abbe18940f2b
SnoopyKim <snoopykim0@gmail.com>
2020-04-13 16:40:23 +0900

    Homework in 01.3

--- DiceRoller/app/src/main/java/android/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/android/example/diceroller/MainActivity.kt
@@ -18,11 +18,19 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val clearButton: Button = findViewById(R.id.clear_button)
+        clearButton.setOnClickListener { clearDice() }
     }
 
     private fun rollDice() {
         diceImage1.setImageResource(getRandomDiceImage())
         diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun clearDice() {
+        diceImage1.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
     }
 
     private fun getRandomDiceImage(): Int {
