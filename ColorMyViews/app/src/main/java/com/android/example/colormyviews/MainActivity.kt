0cc1131665336fa9e4d6c6365202a0da06f75cc5
SnoopyKim <snoopykim0@gmail.com>
2020-04-17 22:58:23 +0900

    Coding challenge 1 in 02.3

--- ColorMyViews/app/src/main/java/com/android/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/android/example/colormyviews/MainActivity.kt
@@ -18,12 +18,12 @@ class MainActivity : AppCompatActivity() {
     private fun makeColored(view: View) {
         when (view.id) {
             // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            else -> view.setBackgroundColor(Color.LTGRAY)
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.image_one)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.image_two)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.image_one)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.image_one)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.image_one)
+            else -> view.setBackgroundResource(R.drawable.image_background)
         }
     }
 
